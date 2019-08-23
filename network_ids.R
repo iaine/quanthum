d49301d8933f8ea009e21bc69ee3e68ffc4c4ec3
iaine <iain.emsley@kellogg.ox.ac.uk>
2019-08-23 20:20:50 +0100

    Added in date and generation constraints

--- network_ids.R
+++ network_ids.R
@@ -1,20 +1,49 @@
 library(igraph)
 
+#todo: colors for type of relations. (Parent as red & child as blue?)
+#edge.col <- V(net)$color[edge.start]
+#plot(net, edge.color=edge.col, edge.curved=.1)
+status_levels <- c("ancestor", "same", "descendent")
+status_colors <- c("#00B050", "#FFC000", "#C00000")
+
 find_numbers <- function (dfA, dfB) {
   a <- unlist(strsplit(as.character(dfA), ';'))
   b <- unlist(strsplit(as.character(dfB), ';'))
   return(length(intersect(a, b)))
 }
 
+#todo: break this up into smaller functions
 find_relations <- function(fA) {
-  relations = data.frame(from=character(), to=character(), weight=numeric(), stringsAsFactors = FALSE)
+  relations = data.frame(from=character(), to=character(), weight=numeric(), linktype=character(), stringsAsFactors = FALSE)
   for (i in (1:length(dat$Ids))) {
     for (j in (1:length(dat$Ids))) {
-      if (j > i) {
+      # remove self referential links
+      # j > i to not count links that already counted
+      if (j > i  && dat$Ids[i] != dat$Ids[j]) {
+        #contentious. Henry4 and Richard2 born in same year but H4 dies after R2. 
+        # How to handle?
+        # Death constraint:  && dat$end[i] == dat$end[j]
+        if ((dat$start[i] - dat$start[j]) < 0) {
+          rels = find_numbers(dat$Ids[i], dat$Ids[j])
+          if (rels > 0) {
+            newVertex <- data.frame(from=dat$Titles[i],to=dat$Titles[j],weight=rels, linktype="#00B050")
+            relations <- rbind(relations, newVertex)
+          }
+        }
+        else if ((dat$start[i] - dat$start[j]) < 30 ) {
         rels = find_numbers(dat$Ids[i], dat$Ids[j])
         if (rels > 0) {
-          newVertex <- data.frame(from=dat$Titles[i],to=dat$Titles[j],weight=rels)
+          newVertex <- data.frame(from=dat$Titles[i],to=dat$Titles[j],weight=rels, linktype="#FFC000")
           relations <- rbind(relations, newVertex)
+        }
+        }
+        else if ((dat$start[i] - dat$start[j]) >= 30 && (dat$start[i] - dat$start[j]) < 500) {
+          # Cap of 150 years to limit 
+          rels = find_numbers(dat$Ids[i], dat$Ids[j])
+          if (rels > 0) {
+            newVertex <- data.frame(from=dat$Titles[i],to=dat$Titles[j],weight=rels, linktype="#C00000")
+            relations <- rbind(relations, newVertex)
+          }
         }
       }
     }
@@ -25,30 +54,26 @@ find_relations <- function(fA) {
 plot_diagram <- function (df_relations , fname) {
   png(fname, width=1080, height = 1080, units = 'px')
   
-  # Load (DIRECTED) graph from data frame 
-  g <- graph.data.frame(df_relations, directed = FALSE)
+  g <- graph.data.frame(df_relations, directed = TRUE)
   
   # Plot graph
-  plot(g, edge.width=E(g)$weight)
-  dev.off()
+  plot(g, edge.width=E(g)$weight, edge.color=E(g)$linktype)
+  legend("topleft",
+         status_levels,
+         fill=status_colors
+  )
+   dev.off()
 }
 
-h<-read.csv('/Users/iain/Desktop/quanthum/characters.csv', header = FALSE, fill = TRUE,stringsAsFactors = FALSE)
+h<-read.csv('/Users/iain/git/quanthum/characters.csv', header = FALSE, fill = TRUE,stringsAsFactors = FALSE)
 
-dat <- data.frame(Titles=h$V1, Ids=h$V2)
+#filter by the history plays.
+h1 <- h[which (h$V1 %in% c('F-r2', 'F-1h4', 'F-2h4', 'F-h5', 'F-1h6', 'F-2h6', 'F-3h6', 'F-r3', 'F-h8', 'F-jc')),]
 
-rel <- find_relations(dat)
-
-plot_diagram(rel, '/Users/iain/Desktop/quanthum/standardnames.png')
-
-dat <- data.frame(Titles=h$V1, Ids=h$V3)
+dat <- data.frame(Titles=h1$V1, Ids=h1$V2, start=h1$V5, end=h1$V6)
 
 rel <- find_relations(dat)
 
-plot_diagram(rel, '/Users/iain/Desktop/quanthum/variancesnames.png')
-
-dat <- data.frame(Titles=h$V1, Ids=h$V4)
-
-rel <- find_relations(dat)
-
-plot_diagram(rel, '/Users/iain/Desktop/quanthum/actorids.png')
+plot_diagram(rel, '/Users/iain/git/quanthum/standardnames.png')
+#plot_diagram(rel, '/Users/iain/git/quanthum/variancesnames.png')
+#plot_diagram(rel, '/Users/iain/git/quanthum/actorids.png')
